2c6da86383bf30292a27beb3ffaba51a316c10b0
Troy <clandaith@gmail.com>
2013-07-28 14:28:00 -0600

    asdf

---
+++ src/com/clandaith/freestars/FreeStars.java
@@ -0,0 +1,21 @@
+package com.clandaith.freestars;
+
+import com.clandaith.freestars.gui.GUI;
+
+/*
+ *
+ * Copyright Troy Davidson
+ * 
+ * Create by Troy Davidson  troy@clandaith.com
+ * Created on Jul 28, 2013 9:44:43 AM
+ *
+ */
+
+public class FreeStars {
+	public static void main(String[] args) {
+
+		new GUI().run();
+
+	}
+
+}
